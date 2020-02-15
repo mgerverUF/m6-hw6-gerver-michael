c3078bf70e4c307d68717f09ff65f20dab27d6ec
Michael Gerver <mgerver@ufl.edu>
2020-02-15 17:36:57 -0500

    Add js code for messages on click

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// BACKGROUND COLOR CHANGE ON HOVER 
 var boxList = document.querySelector('.box-list');
 
 boxList.addEventListener('mouseenter', colorChange)  
@@ -13,3 +14,35 @@ function changeBack() {
     document.getElementById('teal').style.backgroundColor = '#55FFC9';
     document.getElementById('green').style.backgroundColor = '#A5FF01';
 };
+
+// BOX 1 CLICK ACTION https://www.tutorialspoint.com/html-dom-appendchild-method
+var boxOne = document.querySelector('#yellow');
+
+boxOne.addEventListener('click', messageOne)  
+function messageOne() {
+    var clickOne = document.createElement('p');
+    clickOne.innerHTML = 'Ohhh - so close, but no cigar';
+    document.querySelector('.results').appendChild(clickOne);
+};
+
+// BOX 2 CLICK ACTION 
+var boxTwo = document.querySelector('#teal');
+
+boxTwo.addEventListener('click', messageTwo)  
+function messageTwo() {
+    var clickTwo = document.createElement('p');
+    clickTwo.innerHTML = 'DING DING DING - We have a winner';
+    document.querySelector('.results').appendChild(clickTwo);
+};
+
+// BOX 3 CLICK ACTION 
+var boxThree = document.querySelector('#green');
+
+boxThree.addEventListener('click', messageThree)  
+function messageThree() {
+    var clickThree = document.createElement('p');
+    clickThree.innerHTML = 'Oops, better luck next time';
+    document.querySelector('.results').appendChild(clickThree);
+};
+
+
